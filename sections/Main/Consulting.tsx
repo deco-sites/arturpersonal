137554029144d55c919125b22f20ae48e608c74b
Ana Carolina D. C <76822093+devartes@users.noreply.github.com>
2023-10-03 08:32:15 -0300

    feat: add section Notes and remove unnecessary div from Consulting section

--- sections/Main/Consulting.tsx
+++ sections/Main/Consulting.tsx
@@ -18,11 +18,9 @@ export interface Card {
 export default function Consulting({ title, cardService }: ConsultingContent) {
   return (
     <div class="max-w-[1200px] mx-auto">
-      <div class="">
-        <h2 class="text-center pt-[3rem] pb-[2rem] mx-[15px] sm:text-[40px] text-[30px] font-bold">
-          {title}
-        </h2>
-      </div>
+      <h2 class="text-center pt-[3rem] pb-[2rem] mx-[15px] sm:text-[40px] text-[30px] font-bold">
+        {title}
+      </h2>
       <div class="flex md:flex-row flex-col flex-wrap justify-around items-center sm:px-[32px] px-[16px]">
         {cardService?.map((card) => (
           <div class="md:w-[30%] w-full flex flex-col justify-center items-center md:p-[2rem] p-[1rem]">
@@ -35,7 +33,8 @@ export default function Consulting({ title, cardService }: ConsultingContent) {
                   alt={card.label}
                 />
               </figure>
-              <span class="w-full h-[100px] absolute -z-[1] rounded-full mt-[25px] ml-[35px] bg-[#2f80ed]"></span>           
+              <span class="w-full h-[100px] absolute -z-[1] rounded-full mt-[25px] ml-[35px] bg-[#2f80ed]">
+              </span>
             </div>
             <HTMLRenderer html={card.description || ""} class="text-center" />
           </div>
